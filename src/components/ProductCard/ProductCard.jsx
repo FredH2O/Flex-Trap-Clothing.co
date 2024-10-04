f88a430cab1f6ebdff9cce4e13ba1d18bcaf5695
Fred <huertaserick@yahoo.ie>
2024-10-04 18:51:06 +0100

    Fixed product cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,9 +7,10 @@ export default function ProductCard({ image, title, price, description }) {
         <img className="card-img-top" src={image} alt={title} />
         <div className="card-body p-4">
           <div className="text-center">
-            <h5 className="fw-bolder">{title}</h5>€{price}
+            <h5 className="fw-bolder">{title}</h5>
+            <span className="price-tag">€{price}</span>
           </div>
-          <div className="text-center">{description}</div>
+          <div className="text-center description">{description}</div>
         </div>
         <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
           <div className="text-center">
