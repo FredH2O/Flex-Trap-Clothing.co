920a50b9a3afad7e34b79bce09f35d0086248724
Fred <huertaserick@yahoo.ie>
2024-10-05 19:54:45 +0100

    Sign in button

--- src/components/Navbar/SignIn.jsx
+++ src/components/Navbar/SignIn.jsx
@@ -1,24 +1,32 @@
+import "./SignIn.css";
+
 export default function SignIn() {
   return (
-    <>
-      <div class="form-floating mb-3">
-        <input
-          type="email"
-          class="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-        />
-        <label for="floatingInput">Email address</label>
+    <div>
+      <div className="user-pass">
+        <div className="container">
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button>Log In</button>
+          <a>Don't have an account? Sign up!</a>
+        </div>
       </div>
-      <div class="form-floating">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingPassword"
-          placeholder="Password"
-        />
-        <label for="floatingPassword">Password</label>
-      </div>
-    </>
+    </div>
   );
 }
