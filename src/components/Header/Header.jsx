4e93e8e367bf2ab95c873ac3a1c951e3607a8295
Fred <huertaserick@yahoo.ie>
2024-10-06 21:38:20 +0100

    Fixing header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
 import "./Header.css";
+import HeroImages from "./HeroImages";
 
 export default function Header() {
   return (
-    <header className="bg-dark py-5">
-      <div className="container px-4 px-lg-5 my-5">
+    <header className="bg-dark">
+      <div className="container landing-page">
         <div className="text-center text-white">
-          <h1 className="display-4 fw-bolder">Shopping made easy</h1>
-          <p className="lead fw-normal text-white-50 mb-0">
-            Shop till you drop !
-          </p>
+          <HeroImages />
         </div>
       </div>
     </header>
---
+++ src/components/Header/HeroImages.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import imagesData from "./images.json";
+
+export default function HeroImages() {
+  return (
+    <>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {imagesData.map((image, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${index === 0 ? "active" : ""} `}
+            >
+              <img
+                src={require(`../../images/${image.src}`)}
+                className="d-block hero-image"
+                alt={image.alt}
+              />
+              <div className="header-title">
+                <h1 className="display-4 fw-bolder">Shopping made easy</h1>
+                <p className="lead fw-normal text-white-50 mb-0">
+                  Shop till you drop !
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+}
