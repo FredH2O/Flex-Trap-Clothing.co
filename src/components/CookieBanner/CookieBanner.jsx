bae6293fb0f951f8ea1bdc462490572f972d3a80
Fred <huertaserick@yahoo.ie>
2024-10-26 21:46:27 +0100

    Finish

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import "./CookieBanner.css";
 
 function CookieBanner() {
   const [cookies, setCookie] = useCookies(["userConsent"]);
   const hasConsent = cookies.userConsent;
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (hasConsent !== undefined) {
+      setIsVisible(false);
+    }
+  }, [hasConsent]);
 
   function acceptCookies() {
     setCookie("userConsent", true, { path: "/", maxAge: 3600 * 24 * 30 });
+    setIsVisible(false);
   }
 
-  return !hasConsent ? (
+  function rejectCookies() {
+    setCookie("userConsent", false, { path: "/", maxAge: 3600 * 24 * 30 });
+    setIsVisible(false);
+  }
+
+  return isVisible && !hasConsent ? (
     <div className="cookie-banner">
       <p>We use cookies to improve your experience. Do you accept?</p>
-      <button onClick={acceptCookies}>Accept</button>
-      <button>Reject</button>
+      <button className="m-1" onClick={acceptCookies}>
+        Accept
+      </button>
+      <button className="m-1" onClick={rejectCookies}>
+        Reject
+      </button>
     </div>
   ) : null;
 }
