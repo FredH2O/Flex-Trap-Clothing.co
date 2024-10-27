72d02d6615dd28b0ad7ccadd41d8dfe81dc5262f
Fred <huertaserick@yahoo.ie>
2024-10-27 01:20:09 +0100

    test

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <main id="root">
           <Routes>
             <Route
-              path="/home"
+              path="/"
               element={
                 <>
                   <Header />
